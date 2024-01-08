e5484e29486d7dff226e41dc4f7522e297dbbb84
user77 <gamemathstar1@gmail.com>
2024-01-08 20:38:36 +0100

    sfdg

--- resources/views/commons/menu/sidebar.blade.php
+++ resources/views/commons/menu/sidebar.blade.php
@@ -44,31 +44,31 @@
             </a>
         </li>
 
-        <li>
-            <a href="javascript:;" class="side-menu">
-                <div class="side-menu__icon"> <i data-lucide="list"></i> </div>
-                <div class="side-menu__title">
-                    Reports
-                    <div class="side-menu__sub-icon "> <i data-lucide="chevron-down"></i> </div>
-                </div>
-            </a>
-            <ul class="">
-                <li>
-                    <a href="{{route('sectors.index')}}" class="side-menu">
-                        <div class="side-menu__icon"> <i data-lucide="activity"></i> </div>
-                        <div class="side-menu__title"> All Sectors </div>
-                    </a>
-                </li>
-                @foreach(\App\Models\Sector::get() as $sector)
-                    <li>
-                        <a href="{{route('sectors.view',[$sector->id])}}" class="side-menu">
-                            <div class="side-menu__icon"> <i data-lucide="activity"></i> </div>
-                            <div class="side-menu__title"> {{$sector->name}} </div>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </li>
+{{--        <li>--}}
+{{--            <a href="javascript:;" class="side-menu">--}}
+{{--                <div class="side-menu__icon"> <i data-lucide="list"></i> </div>--}}
+{{--                <div class="side-menu__title">--}}
+{{--                    Reports--}}
+{{--                    <div class="side-menu__sub-icon "> <i data-lucide="chevron-down"></i> </div>--}}
+{{--                </div>--}}
+{{--            </a>--}}
+{{--            <ul class="">--}}
+{{--                <li>--}}
+{{--                    <a href="{{route('sectors.index')}}" class="side-menu">--}}
+{{--                        <div class="side-menu__icon"> <i data-lucide="activity"></i> </div>--}}
+{{--                        <div class="side-menu__title"> All Sectors </div>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                @foreach(\App\Models\Sector::get() as $sector)--}}
+{{--                    <li>--}}
+{{--                        <a href="{{route('sectors.view',[$sector->id])}}" class="side-menu">--}}
+{{--                            <div class="side-menu__icon"> <i data-lucide="activity"></i> </div>--}}
+{{--                            <div class="side-menu__title"> {{$sector->name}} </div>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                @endforeach--}}
+{{--            </ul>--}}
+{{--        </li>--}}
     </ul>
 </nav>
 <!-- END: Side Menu -->
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -20,16 +20,11 @@
         <div class="hidden xl:flex flex-col min-h-screen">
             <a href="" class="-intro-x flex items-center pt-5">
                 <img alt="Midone - HTML Admin Template" class="w-6" src="/dist/images/logo.svg">
-                <span class="text-white text-lg ml-3"> Enigma </span>
+                <span class="text-white text-lg ml-3"> JSEPM </span>
             </a>
             <div class="my-auto">
-                <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="/dist/images/illustration.svg">
-                <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
-                    A few more clicks to
-                    <br>
-                    sign in to your account.
-                </div>
-                <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Manage all your commitments</div>
+                <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{asset('jigawa3_map.png')}}">
+
             </div>
         </div>
         <!-- END: Login Info -->
@@ -62,7 +57,7 @@
                     </div>
                     <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                         <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</button>
-                        <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Register</button>
+{{--                        <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Register</button>--}}
                     </div>
                     <div class="intro-x mt-10 xl:mt-24 text-slate-600 dark:text-slate-500 text-center xl:text-left"> By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a> </div>
                 </form>
